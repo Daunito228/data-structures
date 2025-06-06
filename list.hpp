bff0c60205fc427ed970a36c89e64beb2a8b55b0
Daunito228 <rep.gep1945@gmail.com>
2025-06-06 20:28:06 +0300

    added insert for list, methods renaming

--- list.hpp
+++ list.hpp
@@ -20,15 +20,19 @@ public:
     List();
     ~List();
     List& operator=(const List& other);
-    void append(T value);
+    void push_back(T value);
+    void push_front(T value);
     void print();
     size_t getSize();
-    T at(size_t index);
+    T at(size_t index)const;
     void removeNode(size_t index);
     void removeByValue(T value);
     void clear();
     bool isEmpty();
     List* sorted();
+    void pop_back();
+    void pop_front();
+    void insert(T value, size_t index);
 };
 
 //
@@ -54,7 +58,7 @@ List<T>& List<T>::operator=(const List& other){
     this->clear();
     this->sz = other.sz;
     while(curr!=nullptr){
-        this->append(curr->value);
+        this->push_back(curr->value);
         curr=curr->next;
     }
     return *this;
@@ -74,7 +78,7 @@ void List<T>::freeList(){
 }
 
 template<typename T>
-void List<T>::append(T value){
+void List<T>::push_back(T value){
     Node* newNode = new Node;
     newNode->prev=nullptr;
     newNode->next=nullptr;
@@ -90,6 +94,57 @@ void List<T>::append(T value){
     }
     curr->next = newNode;
     newNode->prev=curr;
+}
+
+template<typename T>
+void List<T>::push_front(T value){
+    Node* newNode = new Node;
+    newNode->prev=nullptr;
+    newNode->next=nullptr;
+    newNode->value=value;
+    sz++;
+    if(head==nullptr){
+        head = newNode;
+        return;
+    }
+    newNode->next=head;
+    head = newNode;
+}
+
+template<typename T>
+void List<T>::pop_back(){
+    removeNode(sz-1);
+}
+
+template<typename T>
+void List<T>::pop_front(){
+    removeNode(0);
+}
+
+template<typename T>
+void List<T>::insert(T value, size_t index){
+    if(index >= sz){
+        throw std::out_of_range("Index " + std::to_string(index) + " is out of range");
+    }
+    if(index==0){
+        push_front(value);
+        return;
+    }  
+    if(index==sz-1){
+        push_back(value);
+        return;
+    }
+    Node* curr = head;
+    for(size_t i=0;i<index-1;i++){
+        curr=curr->next;
+    }
+    Node* newNode = new Node;
+    newNode->value=value;
+    newNode->prev=curr;
+    newNode->next=curr->next;
+    curr->next->prev = newNode;
+    curr->next = newNode;
+    sz++;
 }
 
 template<typename T>
@@ -112,7 +167,7 @@ size_t List<T>::getSize(){
 }
 
 template<typename T>
-T List<T>::at(size_t index){
+T List<T>::at(size_t index)const{
     return getNode(index)->value;
 }
 
@@ -197,7 +252,7 @@ List<T>* List<T>::sorted(){
             }
             curr=curr->next;
         }
-        sorted->append(minNum);
+        sorted->push_back(minNum);
         listCp.removeByValue(minNum);
     }
     return sorted;
--- main.cpp
+++ main.cpp
@@ -7,18 +7,5 @@ using namespace std;
 
 int main()
 {
-    List<int> list;
-    int num;
-    int maxNum=-__INT_MAX__;
-    for(int i=0;i<10;i++){
-        cout << i << ") Enter: ";
-        cin >> num;
-        list.append(num);
-    }
-    //list.print();
-    List<int>* sorted = new List<int>;
-    sorted = list.sorted();
-    sorted->print();
-    list.print();
-    return 0;
+    
 }
